4396a5b7481c8b404319c066847fa35fe578e08f
Nicolas Santillan <103286286+NicolasAgustinSantillan@users.noreply.github.com>
2025-08-28 15:22:37 -0300

    add (ojeda.concesionario): panels

--- Source/Ojeda.Concesionario/MainForm.Designer.cs
+++ Source/Ojeda.Concesionario/MainForm.Designer.cs
@@ -1,6 +1,5 @@
 ﻿using System.Drawing.Drawing2D;
 using System.Runtime.InteropServices;
-using System.Runtime.InteropServices;
 
 namespace Ojeda.Concesionario
 {
@@ -74,13 +73,43 @@ namespace Ojeda.Concesionario
         private void InitializeComponent()
         {
             var resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            tableLayoutPanel_ALL = new TableLayoutPanel();
+            panel1 = new Panel();
+            tableLayoutPanel_ALL.SuspendLayout();
             SuspendLayout();
+            // 
+            // tableLayoutPanel_ALL
+            // 
+            tableLayoutPanel_ALL.BackColor = SystemColors.Control;
+            tableLayoutPanel_ALL.ColumnCount = 2;
+            tableLayoutPanel_ALL.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 100F));
+            tableLayoutPanel_ALL.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel_ALL.Controls.Add(panel1, 0, 0);
+            tableLayoutPanel_ALL.Dock = DockStyle.Fill;
+            tableLayoutPanel_ALL.Location = new Point(0, 0);
+            tableLayoutPanel_ALL.Name = "tableLayoutPanel_ALL";
+            tableLayoutPanel_ALL.RowCount = 2;
+            tableLayoutPanel_ALL.RowStyles.Add(new RowStyle(SizeType.Absolute, 40F));
+            tableLayoutPanel_ALL.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel_ALL.Size = new Size(800, 450);
+            tableLayoutPanel_ALL.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            tableLayoutPanel_ALL.SetColumnSpan(panel1, 2);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(1, 1);
+            panel1.Margin = new Padding(1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(798, 38);
+            panel1.TabIndex = 0;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(tableLayoutPanel_ALL);
             FormBorderStyle = FormBorderStyle.None;
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "MainForm";
@@ -88,9 +117,13 @@ namespace Ojeda.Concesionario
             Text = "Concesionario";
             Load += MainForm_Load;
             MouseDown += Form_MouseDown;
+            tableLayoutPanel_ALL.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private TableLayoutPanel tableLayoutPanel_ALL;
+        private Panel panel1;
     }
 }
