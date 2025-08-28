a39db6da82a97fe9d186e26c2e2690164e569357
Nicolas Santillan <103286286+NicolasAgustinSantillan@users.noreply.github.com>
2025-08-28 15:05:44 -0300

    add (ojeda.concesionario): new project

---
+++ Source/Ojeda.Concesionario/MainForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿using System.Drawing.Drawing2D;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices;
+
+namespace Ojeda.Concesionario
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region CustomView
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            int radius = 20; // radio de esquinas
+            GraphicsPath path = new GraphicsPath();
+            path.AddArc(0, 0, radius, radius, 180, 90);
+            path.AddArc(this.Width - radius, 0, radius, radius, 270, 90);
+            path.AddArc(this.Width - radius, this.Height - radius, radius, radius, 0, 90);
+            path.AddArc(0, this.Height - radius, radius, radius, 90, 90);
+            path.CloseAllFigures();
+
+            this.Region = new Region(path);
+
+            // borde
+            using (Pen pen = new Pen(Color.FromArgb(33, 150, 243), 4))
+            {
+                e.Graphics.DrawPath(pen, path);
+            }
+        }
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        private void Form_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(this.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+        #endregion
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            var resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            SuspendLayout();
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Concesionario";
+            Load += MainForm_Load;
+            MouseDown += Form_MouseDown;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ Source/Ojeda.Concesionario/MainForm.cs
@@ -0,0 +1,23 @@
+using System.Drawing.Drawing2D;
+using System.Drawing.Drawing2D;
+
+namespace Ojeda.Concesionario
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+    }
+}
